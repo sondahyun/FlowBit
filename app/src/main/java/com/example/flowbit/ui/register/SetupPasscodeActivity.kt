968afeba62660a6c84fa1baf8364ea59a9151d4a
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-02-17 23:56:12 +0900

    feat: 비밀번호 찾기 기능

--- app/src/main/java/com/example/flowbit/ui/register/SetupPasscodeActivity.kt
+++ app/src/main/java/com/example/flowbit/ui/register/SetupPasscodeActivity.kt
@@ -5,5 +5,6 @@ import com.example.flowbit.databinding.ActivitySetupPasscodeBinding
 
 class SetupPasscodeActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySetupPasscodeBinding
-    
+
+    // 
 }
